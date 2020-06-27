4dc401ab79f2100886d3e29d8188eeea2297397e
paul_walter <paul_walter@medisked.com>
2020-06-27 10:24:33 -0400

    Checked in the .sh, .bat and nswag.json file updates.

--- TodoApi/Automation/GenFiles.bat
+++ TodoApi/Automation/GenFiles.bat
@@ -1,4 +1,4 @@
-﻿@ECHO OFF
+@ECHO OFF
 ECHO Generating NSwag
 ECHO - Generating Swagger documents
 
---
+++ TodoApi/Automation/GenFiles.sh
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/bash
+
+echo "Generating NSwag"
+echo "Generating Swagger documents"
+
+# Create the Swagger Document
+nswag run nswag.json /runtime:NetCore31
+
+# Create the CS Controllers
+echo "Generating CSharp Controllers"
+nswag swagger2csclient /input:Generated\TodoApi.Swagger.json /output:Generated\TodoApi.Generated.txt /namespace:com.walterp.todo
+
+# Create the Typescript Controllers.
+echo "Generating Typescript Files"
+nswag swagger2tsclient /input:Generated\TodoApi.Swagger.json /output:Generated\TodoApi.Generated.txt /namespace:com.walterp.todo
+
+
